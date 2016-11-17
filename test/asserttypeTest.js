e316c0ecccbf8e9f10681127c52c78bc39331677
GaneshSPatil <ganeshpl@thoughtworks.com>
2016-11-17 21:39:54 +0530

    Added tests for boolean assertion

--- test/asserttypeTest.js
+++ test/asserttypeTest.js
@@ -51,4 +51,25 @@ describe('asserttype', () => {
     });
   });
 
+  describe('Boolean', () => {
+    it('should be a function', () => {
+      expect(expect(true).to.be.boolean).to.be.a('function');
+    });
+
+    it('should assert boolean values', () => {
+      expect(true).to.be.boolean();
+      false.should.be.boolean();
+    });
+
+    it('should throw error when it fails to assert boolean values', () => {
+      expect(() => expect(1).to.be.boolean()).to.throw(Error);
+    });
+
+    it('should be false when the actual value is not a boolean', () => {
+      expect(2).not.to.be.boolean();
+      2.0.should.not.be.boolean();
+      expect([1, 2, 3]).not.to.be.boolean();
+      [1, 2, 3].should.not.be.boolean();
+    });
+  });
 });
